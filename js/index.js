6e5652219659a0298dab19ab624e555335519320
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-06-03 15:52:50 -0500

    Elimine listeners pasivos

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-(function () {
-    if (typeof EventTarget !== "undefined") {
-        let func = EventTarget.prototype.addEventListener;
-        EventTarget.prototype.addEventListener = function (type, fn, capture) {
-            this.func = func;
-            if (typeof capture !== "boolean") {
-                capture = capture || {};
-                capture.passive = false;
-            }
-            this.func(type, fn, capture);
-        };
-    }
-})();
-
 const $contactUsForm = document.querySelector("#contatUs-form");
 
 async function handleContactus(evt) {
