dfe835ed6f22d213a781b77d5cc4832c84832970
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-06-03 15:48:11 -0500

    Pasive listener

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+(function () {
+    if (typeof EventTarget !== "undefined") {
+        let func = EventTarget.prototype.addEventListener;
+        EventTarget.prototype.addEventListener = function (type, fn, capture) {
+            this.func = func;
+            if (typeof capture !== "boolean") {
+                capture = capture || {};
+                capture.passive = false;
+            }
+            this.func(type, fn, capture);
+        };
+    }
+})();
+
 const $contactUsForm = document.querySelector("#contatUs-form");
 
 async function handleContactus(evt) {
